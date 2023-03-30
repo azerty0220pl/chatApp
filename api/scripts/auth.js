628101fccc8563db8109c35ac96462192ce89f7c
Szymon Kokot <azerty0220pl@gmail.com>
2023-03-30 18:36:20 +0200

    auth.js implemented

---
+++ api/scripts/auth.js
@@ -0,0 +1,25 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+const dbMan = require('./databaseMan.js');
+
+module.exports = function (app, myDataBase) {
+    passport.serializeUser((user, done) => {
+        done(null, user._id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        done(null, dbMan.getUser(id).user);
+    });
+
+    passport.use(new LocalStrategy((username, password, done) => {
+        let user = dbMan.getUser(username);
+        if(user.status == 'error')
+            return done(user.message);
+        if (!user)
+            return done(null, false);
+        if (!bcrypt.compareSync(password, user.password))
+            return done(null, false);
+        return done(null, user);
+    }));
+}
