e9a6a9a7a64f25576d4a625407afb3d457415520
Szymon Kokot <azerty0220pl@gmail.com>
2023-03-30 17:35:13 +0200

    connect function

--- api/scripts/databaseMan.js
+++ api/scripts/databaseMan.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true });
+const connect = () => mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true });
 
 const Schema = mongoose.Schema;
 
