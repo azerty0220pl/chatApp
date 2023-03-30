54d28b691c71a67de6dd2ea7ead371c4fc2b62db
Szymon Kokot <azerty0220pl@gmail.com>
2023-03-30 21:17:19 +0200

    app.js implementation

--- api/app.js
+++ api/app.js
@@ -1,21 +1,79 @@
 const express = require('express');
 const app = express();
-http = require('http');
+const http = require('http');
 const cors = require('cors');
+const passportSocketIo = require('passport.socketio');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const { Server } = require('socket.io');
-
-app.use(cors());
+const dbMan = require('./scripts/databaseMan.js');
+const auth = require('./scripts/auth.js');
+const routes = require('./scripts/routes.js');
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:4000',
     methods: ['GET', 'POST'],
   },
 });
 
-io.on('connection', (socket) => {
-  
+app.use(cors());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false },
+  key: 'express.sid',
+  store: store
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
+dbMan.connect.then(() => {
+  auth();
+  routes(app);
+
+  io.on('connection', (socket) => {
+
+    if(chats.state == "success") {
+      socket.join(socket.request.user.username);
+      socket.emit('joined');
+    } else {
+      socket.emit('error');
+    }
+
+    io.on('message', (data) => {
+      let x = dbMan.sendMessage(data.from, data.to, data.message);
+      socket.emit('sent', {status: x.status});
+      io.to(data.to).emit('message', data);
+    });
+  });
+}).catch((err) => {
+  console.log("Unable to connect to database: " + err)
 });
 
-server.listen(4000, () => 'Server is running on port 4000');
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection');
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  console.log('failed connection', message);
+  accept(null, false);
+}
+
+server.listen(5000, () => 'Server is running on port 4000');
