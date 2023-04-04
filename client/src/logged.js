25e2dc2f408a6d6b11d85bfeabf3bed48f226d61
Szymon Kokot <azerty0220pl@gmail.com>
2023-04-04 14:00:16 +0200

    redux contact implement

--- client/src/logged.js
+++ client/src/logged.js
@@ -6,7 +6,13 @@ class Logged extends React.Component {
     render() {
         return (
             <div id="logged" className="card d-flex flex-row h-75 w-75 justify-content-evenly">
-                <Contact />
+                <Contact
+                    chats={this.props.chats}
+                    curNum={this.props.curNum}
+                    curName={this.props.curName}
+                    username={this.props.username}
+                    changeChat={this.props.changeChat}
+                    logout={this.props.logout} />
                 <div className="border-end m-1" />
                 <Chat />
             </div>
