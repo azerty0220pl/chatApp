7ead96201241b1fce6f3d47d2a6acd3b990d111a
Szymon Kokot <azerty0220pl@gmail.com>
2023-04-04 13:57:52 +0200

    redux implementation

--- client/src/contact.js
+++ client/src/contact.js
@@ -2,37 +2,39 @@ import React from "react";
 import Axios from 'axios';
 
 class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            write: ''
+        }
+
+        this.handleWrite = this.handleWrite.bind(this);
+    }
+
+    handleWrite(e) {
+        this.setState({write: e.target.value});
+    }
+
     render() {
         return (
             <div className="d-flex flex-column">
-                <div className="d-flex">
-                    <input type="text" className="form-control" />
-                    <button className="btn btn-primary rounded-pill">Write</button>
+                <div>
+                    <h2>{this.props.username}</h2>
+                    <button className="btn btn-danger rounded-pill" onClick={this.props.logout}>LogOut</button>
                 </div>
+                <div className="d-flex">
+                    <input type="text" className="form-control" onChange={this.handleWrite} />
+                    <button className="btn btn-primary rounded-pill" onClick={this.props.changeChat(this.state.write)}>Write</button>
+                </div>
+                <div>
                 {
-                    () => {
-                        Axios({
-                            method: 'GET',
-                            url: 'https://chatapp-api-6dvw.onrender.com/chats'
-                        }).then(res => {
-                            if(res.data.status === "success") {
-                                res.data.chats.forEach(x => {
-                                    if(x.user1 == 'azerty') {
-                                        return (<button>{x.user2}</button>);
-                                    } else {
-                                        return (<button>{x.user1}</button>);
-                                    }
-                                });
-                            } else {
-                                this.setState({error: true});
-                                return <div />
-                            }
-                        }).catch(err => {
-                            console.log(err);
-                            return <div />
-                        })
-                    }
+                    this.props.chats.array.forEach(chat => {
+                        let name = chat.user1 === this.props.username ? chat.user2 : chat.user1;
+                        return <button className="btn" value={name} onClick={this.props.changeChat} />
+                    })
                 }
+                </div>
             </div>
         );
     }
