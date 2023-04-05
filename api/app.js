99133ef220e1eaf236e7958454336c8cf49ac76e
Szymon Kokot <azerty0220pl@gmail.com>
2023-04-05 13:03:34 +0200

    comment supports credentials

--- api/app.js
+++ api/app.js
@@ -13,7 +13,7 @@ const passport = require('passport');
 
 const app = express();
 const server = http.createServer(app);
-cors.SupportsCredentials = true;
+//cors.SupportsCredentials = true;
 const io = new Server(server, {
   cors: {
     origin: 'http://localhost:4000',
