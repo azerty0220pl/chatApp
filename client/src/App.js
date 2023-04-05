f59d49a6aba57141a0229cc85298006d0d71b03f
Szymon Kokot <azerty0220pl@gmail.com>
2023-04-05 11:46:41 +0200

    consolelog cookies

--- client/src/App.js
+++ client/src/App.js
@@ -71,6 +71,7 @@ const reducer = (state = DEFAULT, action) => {
   let res = null;
   switch (action.type) {
     case LOGIN:
+      console.log(document.cookie);
       axios.get("https://chatapp-api-6dvw.onrender.com/chats?username=" + action.username, {
         withCredentials: true,
         credentials: 'include',
